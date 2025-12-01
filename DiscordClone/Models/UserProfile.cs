9aff1b5b1bc7c37354fd08076de3ac0802783321
mcsilk <kubadolak7@gmail.com>
2025-12-01 18:14:33 +0100

    Modele i migracjia

---
+++ DiscordClone/Models/UserProfile.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections;
+
+namespace DiscordClone.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string AvatarURL { get; set; }
+        public string Username { get; set; }
+        public string BIO { get; set; }
+        public int Tag { get; set; }
+
+        // Navigation
+
+        public virtual ApplicationUser User { get; set; }
+
+        public virtual ICollection<Server> Server { get; set; }
+        public virtual ICollection<Message> Messages { get; set; }
+        public virtual ICollection<Friendship> Friendships { get; set; }
+        public virtual ICollection<ServerMember> ServerMembers { get; set; }
+    }
+
+}
